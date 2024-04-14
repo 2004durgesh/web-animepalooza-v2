8e7f647201fd384880fb5126d7d20e20af6c2294
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2024-04-14 20:07:50 +0530

    Update service-worker.js, manifest.json, and next.config.js

--- next.config.js
+++ next.config.js
@@ -2,25 +2,34 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   extendDefaultRuntimeCaching: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable:false,
+  workboxOptions: {
+    disableDevLogs: false,
+  }
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '*',
-        },
-      ],
-    },
-    logging: {
-      fetches: {
-        fullUrl: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
       },
-      level: 'verbose'
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
     },
-});
+    level: 'verbose'
+  },
+}
 
 
-// module.exports = nextConfig
+
+module.exports = withPWA(nextConfig)
