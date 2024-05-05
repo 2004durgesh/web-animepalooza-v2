e38638d6b46ef2ab1e58bc921a3ddaa677e15f36
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2024-05-05 20:17:16 +0530

    remove error from video player

--- src/app/[services]/[provider]/watch/[...episode_id]/page.js
+++ src/app/[services]/[provider]/watch/[...episode_id]/page.js
@@ -7,19 +7,29 @@ const page = async ({ params }) => {
   // await new Promise(resolve => setTimeout(resolve, 5000))
   let services = params.services === 'anime' || params.services === 'manga' ? 'meta' : params.services;
   let provider = params.services === 'anime' || params.services === 'manga' ? "anilist" : params.provider;
+  let tmdbUrl=`https://animepalooza.vercel.app/api/${params.episode_id[0]}?s=${params.episode_id[3]}&e=${params.episode_id[4]}`
+  console.log("tmdburl",tmdbUrl)
   const animeEpisodeLinks = await fetchData(services, provider, `watch/${params.episode_id[0]}`)
-  const moviesEpisodeLinks = params.services === "movies" && provider !== "tmdb"
-    ?
-    await fetchData(services, provider, `watch`, { episodeId: params.episode_id[0], mediaId: `${params.episode_id[1]}/${params.episode_id[2]}` })
-    :
-    await fetch(`https://animepalooza.vercel.app/api/${params.episode_id[0]}?s=${params.episode_id[3]}&e=${params.episode_id[4]}`).then(res => res.json());
+  const moviesEpisodeLinks=await fetchData(services, provider, `watch`, { episodeId: params.episode_id[0], mediaId: `${params.episode_id[1]}/${params.episode_id[2]}` })
+  // const moviesEpisodeLinks = (params.services === "movies" && provider !== "tmdb")
+  //   ?
+  //   await fetchData(services, provider, `watch`, { episodeId: params.episode_id[0], mediaId: `${params.episode_id[1]}/${params.episode_id[2]}` })
+  //   :
+  //   await fetch(tmdbUrl)
+  //   .then(res => {
+  //       console.log(res);
+  //       return res.json();
+  //   })
+  //   .then(data => console.log(data))
+  //   .catch(error => console.error('Error fetching data:', error));
+
   const episodeLinks = params.services === 'anime' ? animeEpisodeLinks : moviesEpisodeLinks;
   const sourceLink = params.services === 'anime' ? episodeLinks.sources[4].url : params.provider === "tmdb" ? episodeLinks.source: episodeLinks.sources[0].url;
   return (
     <Suspense fallback={<Loading />}>
       <main>
         {/* {SearchParams} */}
-        {/* {JSON.stringify(sourceLink)} */}
+        {/* {JSON.stringify(episodeLinks)} */}
         <VideoPlayer sourceLink={sourceLink} services={services}/>
       </main>
     </Suspense>
