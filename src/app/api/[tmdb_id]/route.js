f58a883bf5a43290409be7cc70ea06907166a113
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2024-05-31 11:28:24 +0530

    Change in api route

--- src/app/api/[tmdb_id]/route.js
+++ src/app/api/[tmdb_id]/route.js
@@ -1,55 +1,165 @@
 import { NextResponse } from 'next/server'
-import { getVidsrcSourcesId, getVidsrcSourceDetails, getVidsrcSources, getSubtitles } from '../modules/hooks';
-import { encodeId, generateRandomIp, generateRandomUserAgent, getFutoken } from '../modules/vidplay/utils';
+import { vidsrcBase } from "../src/common.js";
+import { load } from "cheerio";
+import { getVidsrcMovieSourcesId, getVidsrcShowSourcesId, getVidsrcSourceDetails, getVidsrcSources } from "../src/main.js";
+import { encodeId, getFutoken } from "../src/utils.js";
+import axios from "axios";
+import randomUseragent from 'random-useragent';
 
 export async function GET(request, context) {
   const id = context.params.tmdb_id;
   const season = request.nextUrl.searchParams.get("s");
   const episode = request.nextUrl.searchParams.get("e");
-  const sourcesId = await getVidsrcSourcesId(id, season, episode);
+  randomUseragent.getRandom();
 
-  if (!sourcesId) {
-    NextResponse.json({
+  var ip = (Math.floor(Math.random() * 255) + 1) + "." + (Math.floor(Math.random() * 255)) + "." + (Math.floor(Math.random() * 255)) + "." + (Math.floor(Math.random() * 255));
+
+  if (!id) {
+    return NextResponse.json({
       status: 404,
       return: "Oops media not available"
     }, { status: 404 })
-    return;
+
   };
+  //
+  if (!season && !episode) {
+    const sourcesId = await getVidsrcMovieSourcesId(id);
+    if (!sourcesId) {
+      return NextResponse.json({
+        status: 404,
+        return: "Oops media not available"
+      }, { status: 404 })
+    };
+    const sources = await getVidsrcSources(sourcesId);
 
-  const sources = await getVidsrcSources(sourcesId);
-  const vidplay = sources.result.find((v) => v.title.toLowerCase() === 'vidplay');
-  if (!vidplay) NextResponse.json('vidplay stream not found for vidsrc', { status: 404 });
+    const vidplay = sources.data.result.find((v) => v.title.toLowerCase() === 'vidplay');
 
-  const vidplayLink = await getVidsrcSourceDetails(vidplay.id);
+    if (!vidplay) {
+      return NextResponse.json({
+        status: 404,
+        return: "vidplay stream not found for vidsrc"
+      }, { status: 404 })
 
-  const key = await encodeId(vidplayLink.split('/e/')[1].split('?')[0]);
-  const data = await getFutoken(key, vidplayLink);
+    };
 
-  const subtitles = await getSubtitles(vidplayLink)
+    const vidplayLink = await getVidsrcSourceDetails(vidplay.id);
 
-  const response = await (await fetch(`https://vidplay.online/mediainfo/${data}?${vidplayLink.split('?')[1]}&autostart=true`, {
-    headers: {
-      "Origin": generateRandomIp(),
-      "Referer": vidplayLink,
-      "Host": "vidplay.online",
-      "User-Agent": generateRandomUserAgent()
+    const key = await encodeId(vidplayLink.split('/e/')[1].split('?')[0]);
+    const data = await getFutoken(key, vidplayLink);
+
+    let subtitles;
+    //if(vidplayLink.includes('sub.info='))
+    {
+      //const subtitleLink = vidplayLink.split('?sub.info=')[1].split('&')[0];
+      //const subtitlesFetch = await axios.get(decodeURIComponent(subtitleLink));
+      const data = await axios.get(`${vidsrcBase}/embed/movie/${id}`);
+      const doc = load(data.data);
+      const sourcesCode = doc('a[data-id]').attr('data-id');
+      const subtitlesFetch = await axios.get(`https://vidsrc.to/ajax/embed/episode/${sourcesCode}/subtitles`);
+      subtitles = await subtitlesFetch.data;
+      //console.log(sourcesCode)
     }
-  })).json();
+    const response = await axios.get(`https://vidplay.online/mediainfo/${data}?${vidplayLink.split('?')[1]}&autostart=true`, {
+      params: {
+        v: Date.now().toString(),
+      },
+      headers: {
+        "Origin": ip,
+        "Referer": vidplayLink,
+        "Host": "vidplay.online",
+        "User-Agent": randomUseragent
+      }
+    });
 
-  const result = response.result;
+    const result = response.data.result;
 
-  if (!result && typeof result !== 'object') {
-    throw new Error('an error occured');
+    if (!result && typeof result !== 'object') {
+      throw new Error('an error occured');
+    }
+
+    const source = result.sources?.[0]?.file;
+    if (!sourcesId) {
+      return NextResponse.json({
+        status: 404,
+        return: "Oops u reached rate limit of this api"
+      }, { status: 404 })
+    };
+
+    return NextResponse.json({
+      source: source,
+      referer: vidplayLink.split('?')[0],
+      subtitles: subtitles
+    }, { status: 200 })
   }
 
-  const source = result.sources?.[0]?.file;
-  if (!source) NextResponse.json({
-    status: 404,
-    return: "Oops reached rate limit of this api"
-  }, { status: 404 })
-  return NextResponse.json({
-    source: source,
-    referer: vidplayLink.split('?')[0],
-    subtitles: subtitles
-  }, { status: 200 })
+  if (season && episode) {
+    const sourcesId = await getVidsrcShowSourcesId(id, season, episode);
+    if (!sourcesId) {
+      return NextResponse.json({
+        status: 404,
+        return: "Oops media not available"
+      }, { status: 404 })
+    };
+
+    const sources = await getVidsrcSources(sourcesId);
+
+    const vidplay = sources.data.result.find((v) => v.title.toLowerCase() === 'vidplay');
+
+    if (!vidplay) {
+      return NextResponse.json({
+        status: 404,
+        return: "vidplay stream not found for vidsrc"
+      }, { status: 404 })
+
+    };
+
+    const vidplayLink = await getVidsrcSourceDetails(vidplay.id);
+    
+    const key = await encodeId(vidplayLink.split('/e/')[1].split('?')[0]);
+    const data = await getFutoken(key, vidplayLink);
+
+    let subtitles;
+    //if(vidplayLink.includes('sub.info='))
+    {
+        //const subtitleLink = vidplayLink.split('?sub.info=')[1].split('&')[0];
+        //const subtitlesFetch = await axios.get(decodeURIComponent(subtitleLink));
+		const data = await axios.get(`${vidsrcBase}/embed/tv/${id}/${season}/${episode}`);
+        const doc = load(data.data);
+        const sourcesCode = doc('a[data-id]').attr('data-id');
+        const subtitlesFetch = await axios.get(`https://vidsrc.to/ajax/embed/episode/${sourcesCode}/subtitles`);
+        subtitles = await subtitlesFetch.data;
+    }
+
+    const response = await axios.get(`https://vidplay.online/mediainfo/${data}?${vidplayLink.split('?')[1]}&autostart=true`, {
+        params: {
+            v: Date.now().toString(),
+        },
+        headers: {
+			"Origin": ip,
+            "Referer": vidplayLink,
+			"Host": "vidplay.online",
+			"User-Agent": randomUseragent
+        }
+    });
+
+    const result = response.data.result;
+
+    if (!result && typeof result !== 'object') {
+        throw new Error('an error occured');
+    }
+
+    const source = result.sources?.[0]?.file;
+    if (!sourcesId) {
+        return NextResponse.json({
+            status: 404,
+            return: "Oops u reached rate limit of this api"
+        }, { status: 404 })
+    };
+
+    return NextResponse.json({
+        source: source,
+        referer: vidplayLink.split('?')[0],
+        subtitles: subtitles
+    }, { status: 200 })
+  }
 }
